d96122ea18bc648505c413ed77022ff948ebe081
tonyfaros <tonyfaros@gmail.com>
2018-03-10 10:28:49 -0600

    tetris falta rotar y quitar fila llena

---
+++ app/src/main/java/com/example/anthony_pc/tetris/MainActivity.java
@@ -0,0 +1,334 @@
+package com.example.anthony_pc.tetris;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.Random;
+
+import static android.widget.ImageView.ScaleType.CENTER;
+import static android.widget.ImageView.ScaleType.FIT_XY;
+
+public class MainActivity extends AppCompatActivity {
+    int[][] logicMatrix = new int[20][12];
+    GridLayout gridLayout;
+    Boolean blockOnCourse = false;
+    int blockPlaying = 0;
+    View v1 = null;
+    String blockColorPlaying = "block_blue";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        gridLayout = findViewById(R.id.gridLY);
+        setMatrix();
+        v1 = getWindow().getDecorView().getRootView();
+        setPiezaAzul();
+        final Handler handler = new Handler();
+        final Runnable r = new Runnable() {
+            public void run() {
+
+                if(blockOnCourse) {
+                    Log.e("adsf", "dsf");
+                    bajarPieza(null);
+                }else{
+                    setPiezaAzul();
+                }
+                handler.postDelayed(this, 500);
+            }
+        };
+
+        handler.postDelayed(r, 500);
+
+    }
+
+    public void setMatrix(){
+
+
+        for(int i = 0; i<20; i++){
+            for(int j = 0; j<12; j++) {
+                if (i == 0 || j == 0 || j == 11 || i == 19) {
+                    ImageView imgview = new ImageView(this);
+                    imgview.setImageResource(R.drawable.bloque);
+                    logicMatrix[i][j] = -1;
+                    gridLayout.addView(imgview);
+                }else{
+                    logicMatrix[i][j] = 1;
+                    ImageView imgview = new ImageView(this);
+                    imgview.setTag(String.valueOf(i)+","+String.valueOf(j));
+                    Log.e("tag",imgview.getTag().toString());
+                    gridLayout.addView(imgview);
+                }
+            }
+        }
+    }
+
+    public void setAllUnavlbl(){
+        for(int i = 19; i>0; i--){
+            for(int j = 11; j>0; j--) {
+                if(logicMatrix[i][j] == blockPlaying){
+                    logicMatrix[i][j] = -1;
+                }
+            }
+        }
+    }
+
+    public boolean checkWreckDown(){
+        boolean flag = true;
+        printLogicMatrix();
+        for(int i = 19; i>0; i--){
+            for(int j = 11; j>0; j--){
+                if(logicMatrix[i][j] == blockPlaying && (logicMatrix[i+1][j] == -1)) {
+                    flag = false;
+                    setAllUnavlbl();
+                }
+            }
+        }
+        if(!flag)
+            blockOnCourse = false;
+        return flag;
+    }
+
+    public boolean checkWreckRight(){
+        for(int i = 0; i<20; i++){
+            for(int j = 0; j<12; j++) {
+                if(logicMatrix[i][j] == blockPlaying && logicMatrix[i][j+1] == -1){
+                    return false;}}
+        }
+        return true;
+    }
+
+    public boolean checkWreckLeft(){
+
+         for(int i = 0; i<20; i++){
+            for(int j = 0; j<12; j++) {
+                if(logicMatrix[i][j] == blockPlaying && logicMatrix[i][j-1] == -1){
+                    return false;}}
+        }
+        return true;
+    }
+
+    public void setPiezaAzul(){
+        ImageView image1 = (ImageView)gridLayout.getChildAt(17);
+        ImageView image2 = (ImageView)gridLayout.getChildAt(18);
+        ImageView image3 = (ImageView)gridLayout.getChildAt(29);
+        ImageView image4 = (ImageView)gridLayout.getChildAt(30);
+        ImageView image5 = (ImageView)gridLayout.getChildAt(41);
+        ImageView image6 = (ImageView)gridLayout.getChildAt(42);
+        ImageView image7 = (ImageView)gridLayout.getChildAt(53);
+        ImageView image8 = (ImageView)gridLayout.getChildAt(28);
+
+        switch(random(1,6)){
+            case 1:
+
+                //square  2
+                image1.setImageResource(R.drawable.block_blue);
+                logicMatrix[1][5] = 2;
+                image2.setImageResource(R.drawable.block_blue);
+                logicMatrix[1][6] = 2;
+                image3.setImageResource(R.drawable.block_blue);
+                logicMatrix[2][5] = 2;
+                image4.setImageResource(R.drawable.block_blue);
+                logicMatrix[2][6] = 2;
+                blockOnCourse = true;
+                blockPlaying = 2;
+                blockColorPlaying = "block_blue";
+
+                break;
+            case 2:
+                //L 3
+                blockOnCourse = true;
+                image1.setImageResource(R.drawable.block_yelow);
+                logicMatrix[1][5] = 3;
+                image3.setImageResource(R.drawable.block_yelow);
+                logicMatrix[2][5] = 3;
+                image5.setImageResource(R.drawable.block_yelow);
+                logicMatrix[3][5] = 3;
+                image6.setImageResource(R.drawable.block_yelow);
+                logicMatrix[3][6] = 3;
+                blockPlaying = 3;
+                blockColorPlaying = "block_yelow";
+                break;
+            case 3:
+                //Stick 4
+                image1.setImageResource(R.drawable.block_lightblue);
+                logicMatrix[1][5] = 4;
+                image3.setImageResource(R.drawable.block_lightblue);
+                logicMatrix[2][5] = 4;
+                image5.setImageResource(R.drawable.block_lightblue);
+                logicMatrix[3][5] = 4;
+                image7.setImageResource(R.drawable.block_lightblue);
+                logicMatrix[4][5] = 4;
+                blockOnCourse = true;
+                blockPlaying = 4;
+                blockColorPlaying = "block_lightblue";
+
+                break;
+            case 4:
+                //Lighting  5
+
+                image1.setImageResource(R.drawable.block_green);
+                logicMatrix[1][5] = 5;
+                image3.setImageResource(R.drawable.block_green);
+                logicMatrix[2][5] = 5;
+                image4.setImageResource(R.drawable.block_green);
+                logicMatrix[2][6] = 5;
+                image6.setImageResource(R.drawable.block_green);
+                logicMatrix[3][6] = 5;
+                blockOnCourse = true;
+                blockPlaying = 5;
+                blockColorPlaying = "block_green";
+                break;
+            case 5:
+                //Tank 6
+                image1.setImageResource(R.drawable.block_red);
+                logicMatrix[1][5] = 6;
+                image3.setImageResource(R.drawable.block_red);
+                logicMatrix[2][5] = 6;
+                image4.setImageResource(R.drawable.block_red);
+                logicMatrix[2][6] = 6;
+                image8.setImageResource(R.drawable.block_red);
+                logicMatrix[2][4] = 6;
+                blockOnCourse = true;
+                blockPlaying = 6;
+                blockColorPlaying = "block_red";
+
+                break;
+            case 6:
+                // HarryPotter Scar 7
+
+                image2.setImageResource(R.drawable.block_pink);
+                logicMatrix[1][6] = 7;
+                image3.setImageResource(R.drawable.block_pink);
+                logicMatrix[2][5] = 7;
+                image4.setImageResource(R.drawable.block_pink);
+                logicMatrix[2][6] = 7;
+                image5.setImageResource(R.drawable.block_pink);
+                logicMatrix[3][5] = 7;
+                blockOnCourse = true;
+                blockPlaying = 7;
+                blockColorPlaying = "block_pink";
+                break;
+        }
+    }
+
+    public int random(int i, int j){
+        Random rand = new Random();
+        int n = rand.nextInt(j-i+1)+i; // Gives n such that 0 <= n < 20
+        return n;
+    }
+
+    public void downAllPiece(){
+        printLogicMatrix();
+        for(int k = 19;k>0;k--){
+            for(int n = 11;n>0;n--){
+                if(logicMatrix[k][n] == blockPlaying){
+                    logicMatrix[k+1][n] = blockPlaying;
+                    logicMatrix[k][n] = 1;
+                    ImageView imageView = v1.findViewWithTag(String.valueOf(k)+","+String.valueOf(n));
+                    ImageView imageView2 = v1.findViewWithTag(String.valueOf(k+1)+","+String.valueOf(n));
+                    imageView.setImageResource(0);
+                    imageView2.setImageResource(getResources().getIdentifier(blockColorPlaying, "drawable", getPackageName()));
+                }
+            }
+        }
+    }
+
+    public void bajarPieza(View view){
+        for(int i = 19; i>0; i--){
+            for(int j = 11; j>0; j--) {
+                if(logicMatrix[i][j] == blockPlaying) {
+                    if (checkWreckDown()) {
+                        downAllPiece();
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public void moveRight(View view){
+        if(checkWreckRight()){
+            for(int i = 19; i>0; i--){
+                for(int j = 11; j>0; j--) {
+                    if (logicMatrix[i][j] == blockPlaying){
+                            logicMatrix[i][j+1] = blockPlaying;
+                            logicMatrix[i][j] = 1;
+                            ImageView imageView = v1.findViewWithTag(String.valueOf(i)+","+String.valueOf(j));
+                            ImageView imageView2 = v1.findViewWithTag(String.valueOf(i)+","+String.valueOf(j+1));
+                            imageView.setImageResource(0);
+                        imageView2.setImageResource(getResources().getIdentifier(blockColorPlaying, "drawable", getPackageName()));
+                    }
+                }
+            }
+        }
+    }
+
+    public void moveLeft(View view){
+        if(checkWreckLeft()) {
+            for (int i = 0; i < 20; i++) {
+                for (int j = 0; j < 12; j++) {
+                    if (logicMatrix[i][j] == blockPlaying) {
+                        logicMatrix[i][j - 1] = blockPlaying;
+                        logicMatrix[i][j] = 1;
+                        ImageView imageView = v1.findViewWithTag(String.valueOf(i) + "," + String.valueOf(j));
+                        ImageView imageView2 = v1.findViewWithTag(String.valueOf(i) + "," + String.valueOf(j - 1));
+                        imageView.setImageResource(0);
+                        imageView2.setImageResource(getResources().getIdentifier(blockColorPlaying, "drawable", getPackageName()));
+                    }
+                }
+            }
+        }
+    }
+
+    public void rotateBlock(View view){
+        switch (blockPlaying){
+            case 3:
+                rotateL();
+                break;
+            case 4:
+                rotateStick();
+                break;
+            case 5:
+                rotateLightning();
+                break;
+            case 6:
+                rotateTank();
+                break;
+            case 7:
+                rotateHP();
+                break;
+        }
+    }
+
+    public void rotateL(){
+
+    }
+    public void rotateTank(){
+
+    }
+    public void rotateStick(){
+
+    }
+    public void rotateLightning(){
+
+    }
+    public void rotateHP(){
+
+    }
+
+    public void printLogicMatrix(){
+        for(int i = 0; i<20; i++){
+            for(int j = 0; j<12; j++) {
+                System.out.print(logicMatrix[i][j]);}
+            System.out.print("\n");}
+
+    }
+}
